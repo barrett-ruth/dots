ef56074efd6775cb1e746d2a586a44cabd3a3fb8
Barrett Ruth <br@barrett-ruth.com>
2022-03-20 20:38:57 -0500

    feat: silence session commands

--- nvim/lua/plug/session.lua
+++ nvim/lua/plug/session.lua
@@ -6,6 +6,6 @@ local utils = require 'utils'
 local map = utils.map
 local mapstr = utils.mapstr
 
-map { 'n', '<leader>sd', mapstr 'DeleteSession' }
-map { 'n', '<leader>sr', mapstr 'RestoreSession' }
-map { 'n', '<leader>ss', mapstr 'SaveSession' }
+map { 'n', '<leader>sd', mapstr 'sil DeleteSession' }
+map { 'n', '<leader>sr', mapstr 'sil RestoreSession' }
+map { 'n', '<leader>ss', mapstr 'sil SaveSession' }
