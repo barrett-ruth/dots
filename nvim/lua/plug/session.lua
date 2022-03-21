b7ac1e89a5eeba466b31a27cc47e04cea6846902
Barrett Ruth <br@barrett-ruth.com>
2022-03-20 20:41:13 -0500

    neovim: session notifications on change

--- nvim/lua/plug/session.lua
+++ nvim/lua/plug/session.lua
@@ -6,6 +6,6 @@ local utils = require 'utils'
 local map = utils.map
 local mapstr = utils.mapstr
 
-map { 'n', '<leader>sd', mapstr 'sil DeleteSession' }
-map { 'n', '<leader>sr', mapstr 'sil RestoreSession' }
-map { 'n', '<leader>ss', mapstr 'sil SaveSession' }
+map { 'n', '<leader>sd', mapstr 'sil DeleteSession' .. mapstr "echo 'Session deleted.'" }
+map { 'n', '<leader>sr', mapstr 'sil RestoreSession' .. mapstr "echo 'Session restored.'" }
+map { 'n', '<leader>ss', mapstr 'sil SaveSession' .. mapstr "echo 'Session saved.'" }
