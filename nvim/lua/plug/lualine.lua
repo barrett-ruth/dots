d39c2bf900f7ab621c13508a0f2e7e7fe92278f4
Barrett Ruth <br@barrett-ruth.com>
2022-06-06 13:59:09 -0500

    xmodmap fix super_l

--- nvim/lua/plug/lualine.lua
+++ nvim/lua/plug/lualine.lua
@@ -22,26 +22,24 @@ require('lualine').setup {
             },
             {
                 'diagnostics',
-                symbols = { error = '>', warn = '-', info = ':', hint = '*' },
                 diagnostics_color = {
                     error = { fg = 'ea6962' },
                     warn = { fg = 'd8a657' },
                     info = { fg = '7daea3' },
                     hint = { fg = 'a9b665' },
                 },
+                symbols = {
+                    error = '>',
+                    warn = '-',
+                    info = ':',
+                    hint = '*',
+                },
+                update_in_insert = true,
             },
         },
         lualine_c = { '%F' },
         lualine_x = { lspname, 'filetype' },
         lualine_y = { 'filesize', '%l/%L' },
         lualine_z = { 'encoding', 'bo:ff' },
-    },
-    inactive_sections = {
-        lualine_a = { '%F' },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = { '%l/%L', 'encoding', 'bo:ff' },
     },
 }
