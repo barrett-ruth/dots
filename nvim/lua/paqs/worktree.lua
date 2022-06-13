afead66887961d7722797777e01de20149b130de
Barrett Ruth <br@barrett-ruth.com>
2022-06-13 15:24:56 -0500

    yay worktrees

--- nvim/lua/paqs/worktree.lua
+++ nvim/lua/paqs/worktree.lua
@@ -11,17 +11,11 @@ end
 
 local delete_worktree = function(selected, _)
     local parsed = parse_entry(selected[1])
-
-    require('git-worktree').delete_worktree(parsed.branch)
-end
-
-local force_delete_worktree = function(selected, _)
-    local parsed = parse_entry(selected[1])
     vim.ui.input({
-        prompt = string.format('Force delete worktree %s? [y/N] ', parsed.branch),
+        prompt = string.format('Delete worktree %s? [y/N] ', parsed.branch),
     }, function(input)
         if vim.trim(input):lower() == 'y' then
-            require('git-worktree').delete_worktree(parsed.branch, false)
+            require('git-worktree').delete_worktree(parsed.branch, true)
         end
     end)
 end
@@ -31,7 +25,10 @@ local actions = require 'fzf-lua.actions'
 
 local create_worktree = function(_)
     local query = fzf.config.__resume_data.last_query
-    if require 'utils'.empty(query) then return end
+    if require('utils').empty(query) then
+        return
+    end
+
     local path, branch, upstream = unpack(vim.split(vim.trim(query), ' '))
     branch = not branch and vim.fn.fnamemodify(path, ':t') or branch
 
@@ -39,15 +36,14 @@ local create_worktree = function(_)
 end
 
 return {
-    git_worktrees = function(opts)
-        opts = fzf.config.normalize_opts(opts, fzf.config.globals.git)
+    git_worktrees = function()
+        local opts = fzf.config.normalize_opts({}, fzf.config.globals.git)
 
         opts.cmd = fzf.path.git_cwd('git worktree list | sed "s|$HOME|~|g"', opts)
 
         opts.actions = {
             ['default'] = switch_worktree,
             ['ctrl-a'] = { create_worktree, actions.resume },
-            ['ctrl-f'] = { force_delete_worktree, actions.resume },
             ['ctrl-d'] = { delete_worktree, actions.resume },
         }
 
