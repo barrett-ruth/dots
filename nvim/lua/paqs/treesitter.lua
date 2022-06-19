5154d3503cf930b0c162443064a8ebfc58697b21
Barrett Ruth <br@barrettruth.com>
2022-06-19 02:30:15 -0500

    nvim: treesitter moving

--- nvim/lua/paqs/treesitter.lua
+++ nvim/lua/paqs/treesitter.lua
@@ -1,22 +1,58 @@
-local treesitter_configs = require 'nvim-treesitter.configs'
+local ensure_installed = {}
+local ecma_folds = { 'arrow_function', 'class_declaration', 'function', 'function_declaration', 'method_definition' }
+local treesitter_folds = {
+    bash = { 'function_definition' },
+    c = { 'switch_statement', 'enum_specifier', 'function_definition', 'struct_specifier' },
+    cpp = { 'class_specifier', 'namespace_definition' },
+    'css',
+    'dockerfile',
+    'go',
+    'html',
+    'http',
+    'java',
+    javascript = ecma_folds,
+    'json',
+    lua = { 'function_declaration', 'function_definition' },
+    'make',
+    python = { 'class_definition', 'function_definition' },
+    tsx = ecma_folds,
+    typescript = ecma_folds,
+    vim = { 'function_definition' },
+    'yaml',
+}
+
+for filetype, fold_types in pairs(treesitter_folds) do
+    if type(filetype) == 'string' then
+        table.insert(ensure_installed, filetype)
+
+        local formatted = {}
+        for _, v in ipairs(fold_types) do
+            table.insert(formatted, '(' .. v .. ')')
+        end
+
+        vim.treesitter.set_query(filetype, 'folds', string.format('[%s]@fold', table.concat(formatted)))
+    else
+        table.insert(ensure_installed, fold_types)
+    end
+end
+
+require('nvim-treesitter.configs').setup {
+    ensure_installed = ensure_installed,
+    sync_install = false,
+    indent = {
+        enable = false,
+    },
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+    },
+}
 
 require('nvim-treesitter.configs').setup {
     textobjects = {
         move = {
             enable = true,
             set_jumps = true,
-            goto_next_start = {
-                [']a'] = '@parameter.inner',
-                [']c'] = '@class.outer',
-                [']f'] = '@function.outer',
-                [']i'] = '@conditional.outer',
-            },
-            goto_previous_start = {
-                ['[a'] = '@parameter.outer',
-                ['[c'] = '@class.outer',
-                ['[f'] = '@function.outer',
-                ['[i'] = '@conditional.outer',
-            },
         },
         select = {
             enable = true,
@@ -41,57 +77,6 @@ require('nvim-treesitter.configs').setup {
     },
 }
 
-local ecma_folds = { 'arrow_function', 'class_declaration', 'function', 'function_declaration', 'method_definition' }
-local treesitter_folds = {
-    bash = { 'function_definition' },
-    c = { 'switch_statement', 'enum_specifier', 'function_definition', 'struct_specifier' },
-    cpp = { 'class_specifier', 'namespace_definition' },
-    'css',
-    'dockerfile',
-    'go',
-    'html',
-    'http',
-    'java',
-    javascript = ecma_folds,
-    'json',
-    lua = { 'function_declaration', 'function_definition' },
-    'make',
-    python = { 'class_definition', 'function_definition' },
-    tsx = ecma_folds,
-    typescript = ecma_folds,
-    vim = { 'function_definition' },
-    'yaml',
-}
-
-local ensure_installed = {}
-
-for filetype, fold_types in pairs(treesitter_folds) do
-    if type(filetype) == 'string' then
-        table.insert(ensure_installed, filetype)
-
-        local formatted = {}
-        for _, v in ipairs(fold_types) do
-            table.insert(formatted, '(' .. v .. ')')
-        end
-
-        vim.treesitter.set_query(filetype, 'folds', string.format('[%s]@fold', table.concat(formatted)))
-    else
-        table.insert(ensure_installed, fold_types)
-    end
-end
-
-treesitter_configs.setup {
-    ensure_installed = ensure_installed,
-    sync_install = false,
-    indent = {
-        enable = false,
-    },
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = true,
-    },
-}
-
 local utils = require 'utils'
 local map = utils.map
 
@@ -104,5 +89,29 @@ map {
         require 'paqs.treesitter'
         require 'nvim-treesitter'
         vim.cmd 'e'
+    end,
+}
+
+for k, v in pairs { a = '@parameter.inner', c = '@class.outer', f = '@function.outer', i = '@conditional.outer' } do
+    map(
+        { 'n', ']' .. k, [[':<c-u>lua require("paqs.treesitter").next("]] .. v .. [[", ' . v:count1 . ')<cr>']] },
+        { expr = true }
+    )
+    map(
+        { 'n', '[' .. k, [[':<c-u>lua require("paqs.treesitter").previous("]] .. v .. [[", ' . v:count1 . ')<cr>']] },
+        { expr = true }
+    )
+end
+
+return {
+    next = function(type, count)
+        for _ = 1, count do
+            require('nvim-treesitter.textobjects.move').goto_next_start(type)
+        end
+    end,
+    previous = function(type, count)
+        for _ = 1, count do
+            require('nvim-treesitter.textobjects.move').goto_previous_start(type)
+        end
     end,
 }
