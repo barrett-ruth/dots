71c2057a696875409c3662d03103573660982ff7
Barrett Ruth <br@barrettruth.com>
2022-07-02 18:10:34 -0500

    tmux: use plugins

--- nvim/lua/paqs/gitsigns.lua
+++ nvim/lua/paqs/gitsigns.lua
@@ -1,18 +1,8 @@
 require('gitsigns').setup {
-    signs = {
-        add = { text = '▎' },
-        change = { text = '▎' },
-        delete = { text = '_' },
-        topdelete = { text = '—' },
-        changedelete = { text = '▎' },
-    },
-    update_debounce = 0,
+    current_line_blame = true,
     current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d>',
     current_line_blame_opts = {
         delay = 0,
-    },
-    preview_config = {
-        border = 'single',
     },
     on_attach = function(_)
         local signcolumn = vim.wo.signcolumn
@@ -25,7 +15,7 @@ require('gitsigns').setup {
         local utils = require 'utils'
         local bmap, map, mapstr = utils.bmap, utils.map, utils.mapstr
 
-        bmap { 'n', '<leader>gb', mapstr 'Gitsigns blame_line' }
+        bmap { 'n', '<leader>gb', mapstr 'Gitsigns toggle_current_blame_line' }
         bmap { 'n', '<leader>gp', mapstr 'Gitsigns preview_hunk' }
         bmap { 'n', '[g', mapstr 'Gitsigns prev_hunk' }
         bmap { 'n', ']g', mapstr 'Gitsigns next_hunk' }
@@ -38,4 +28,8 @@ require('gitsigns').setup {
 
         require 'paqs.fugitive'
     end,
+    preview_config = {
+        border = 'single',
+    },
+    update_debounce = 0,
 }
--- nvim/plugin/theme.vim
+++ nvim/plugin/theme.vim
@@ -5,3 +5,9 @@ sign define DiagnosticSignError text=> texthl=DiagnosticError
 sign define DiagnosticSignWarn text=— texthl=DiagnosticWarn
 sign define DiagnosticSignHint text=* texthl=DiagnosticHint
 sign define DiagnosticSignInfo text=: texthl=DiagnosticInfo
+
+sign define GitSignsAdd text=▎
+sign define GitSignsChange text=▎
+sign define GitSignsChangedelete=~
+sign define GitSignsDelete text=_
+sign define GitSignsTopdelete text=‾
