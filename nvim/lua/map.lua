9cb01490550e7119afa77536c57b4c3d6a075ad8
Barrett Ruth <62671086+barrett-ruth@users.noreply.github.com>
2022-07-24 17:59:45 +0530

    Update map.lua

--- nvim/lua/map.lua
+++ nvim/lua/map.lua
@@ -28,7 +28,7 @@ map { 'n', '<tab>', '<c-w>' }
 -- :]
 
 -- Miscellaneous [:
-map { 'n', '<leader><cr>', mapstr 'so %' }
+map { 'n', '<leader><cr>', mapstr 'so' }
 map { 'n', '<leader>-', 'S<esc>' }
 map { 'n', 'ga', '<Plug>(EasyAlign)' }
 map { 'x', 'ga', '<Plug>(EasyAlign)' }
