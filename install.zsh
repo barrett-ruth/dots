01dda2b123f7de669c72b3fb380347e827add0b3
Barrett Ruth <62671086+barrett-ruth@users.noreply.github.com>
2022-09-03 19:40:32 -0500

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -96,6 +96,7 @@ run 'systemctl start dhcpcd.service'
 
 # Configure grub
 run "sed -i '/^#GRUB_DISABLE_OS_PROBER=false/ s/^#*//' /etc/default/grub"
+run "sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT/ s/\"$/ ibt=off\"/' /etc/default/grub"
 run 'grub-install --target=x86_64-efi --bootloader-id=grub --recheck'
 [[ "$(run 'grub-mkconfig -o /boot/grub/grub.cfg' | grep Windows)" ]] || echo ' ***!!! Windows not found in grub install !!!*** '
 
