0d94078a9ad1b170dbeecf2c18da54032b26d2c9
Barrett Ruth <br@barrettruth.com>
2022-10-01 16:06:23 -0400

    chromium: add extensions

--- wip/install.zsh
+++ wip/install.zsh
@@ -339,7 +339,7 @@ rm "$HOME/.yarnrc"
 
 
 post() {
-run 'doas pacman -S clang dash docker docker-compose exa fakeroot harfbuzz tree-sitter fd gcc go google-java-format jdk-openjdk jdtls imlib2 libxft libxinerama libglvnd light lua-language-server make openssh patch pkgconf postgresql python ripgrep rustup shfmt tmux ttf-hanazono ttf-liberation xorg-server xorg-setxkbmap xorg-xinit xorg-xmodmap xorg-xrandr xorg-xrdb xorg-xset which xclip yarn pulseaudio mpv'
+run 'doas pacman -S clang dash docker docker-compose exa fakeroot harfbuzz tree-sitter fd gcc go google-java-format jdk-openjdk jdtls libxft libxinerama libglvnd light lua-language-server make openssh patch pkgconf postgresql python ripgrep rustup shfmt tmux ttf-hanazono ttf-liberation xorg-server xorg-setxkbmap xorg-xinit xorg-xmodmap xorg-xrandr xorg-xrdb xorg-xset which xclip yarn pulseaudio mpv'
 
 run 'doas usermod -aG docker,light "$(whoami)"'
 
