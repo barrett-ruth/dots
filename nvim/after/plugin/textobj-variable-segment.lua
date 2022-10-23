57b78dee18d0f538bb9e14beaea2f710e56cefaa
Barrett Ruth <br@beirut.dev>
2022-10-23 19:20:03 -0400

    nvim: map sentence to S in operator pending

--- nvim/after/plugin/textobj-variable-segment.lua
+++ nvim/after/plugin/textobj-variable-segment.lua
@@ -1,3 +1,6 @@
+vim.keymap.set('o', 'iS', 'is', { noremap = true })
+vim.keymap.set('o', 'aS', 'as', { noremap = true })
+
 vim.g.loaded_textobj_variable_segment = 1
 vim.fn['textobj#user#plugin']('variable', {
     ['-'] = {
