b5c9bc33a71246484cd63bd1ce84533cbf128c61
Barrett Ruth <br@beirut.dev>
2022-11-02 11:51:50 -0400

    nvim: no null-ls files

--- nvim/.null-ls_681471_a.py
+++
@@ -1,9 +0,0 @@
- > /usr/bin/python ~/.config/nvim/a.py
-
-  File "/home/beirut/.config/nvim/a.py", line 1
-    > /usr/bin/python ~/.config/nvim/a.py
-IndentationError: unexpected indent
-
-yes
-
-[DONE] exited with code=0 in 17.377ms
