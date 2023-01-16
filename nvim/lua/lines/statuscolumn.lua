693a7b42f46228e2ab3328cc6f855b6e454806ae
Barrett Ruth <barrettruthtots@gmail.com>
2023-01-15 23:48:16 -0600

    nvim: no click

--- nvim/lua/lines/statuscolumn.lua
+++ nvim/lua/lines/statuscolumn.lua
@@ -1,19 +1,6 @@
 local fold =
     '%#FoldColumn#%{foldlevel(v:lnum)>foldlevel(v:lnum-1)?(foldclosed(v:lnum)==-1?"v ":"> "):" "}'
 
-_G.click = function()
-    local lnum = vim.fn.getmousepos().line
-
-    -- Only lines with a mark should be clickable
-    if vim.fn.foldlevel(lnum) <= vim.fn.foldlevel(lnum - 1) then
-        return
-    end
-
-    vim.cmd(
-        lnum .. 'fold' .. (vim.fn.foldclosed(lnum) == -1 and 'close' or 'open')
-    )
-end
-
 return {
     num = function()
         if vim.v.relnum == 0 then
@@ -23,9 +10,7 @@ return {
         return '%#LineNr#' .. vim.v.relnum
     end,
     statuscolumn = function()
-        return '%@v:lua.click@'
-            .. fold
-            .. '%s%='
-            .. '%{%v:lua.require("lines.statuscolumn").num()%} '
+        return
+            fold .. '%s%=' .. '%{%v:lua.require("lines.statuscolumn").num()%} '
     end,
 }
