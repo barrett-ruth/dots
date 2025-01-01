b9f8052ada48e8e9ad27576a7f04c8758238ba30
Barrett Ruth <br.barrettruth@gmail.com>
2025-01-01 14:56:00 -0600

    final of neovim competitive programming config

--- nvim/lua/cp.lua
+++ nvim/lua/cp.lua
@@ -1,13 +1,16 @@
 vim.api.nvim_create_user_command('CP', function()
-    vim.cmd('45vsplit output.txt')
+    vim.diagnostic.enable(false)
+
+    vim.cmd('50vsplit output.txt')
     vim.cmd.split('input.txt')
     vim.cmd.wincmd('h')
     vim.cmd('vertical resize +8')
+
     vim.api.nvim_create_autocmd('BufWritePost', {
         buffer = vim.api.nvim_get_current_buf(),
         callback = function()
             vim.fn.system(
-                ('sh -c "g++ %s 2>output.txt && test -f a.out && ./a.out >output.txt"'):format(
+                ('sh -c "g++ %s 2>output.txt && test -f a.out && ./a.out >output.txt && rm a.out"'):format(
                     vim.fn.expand('%')
                 )
             )
