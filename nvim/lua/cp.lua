13e2c55dad178a0676653db951a0b08b19560a44
Barrett Ruth <br.barrettruth@gmail.com>
2025-01-04 15:18:09 -0600

    feat(nvim): improve cp setup

--- nvim/lua/cp.lua
+++ nvim/lua/cp.lua
@@ -5,12 +5,45 @@ local function clearcol()
     vim.api.nvim_set_option_value('equalalways', false, { scope = 'global' })
 end
 
-vim.api.nvim_create_user_command('CP', function()
-    vim.diagnostic.enable(false)
+local compile_flags = {
+    '-O2',
+    '-march=native',
+    '-std=c++17',
+    '-Wall',
+    '-Wextra',
+    '-Wshadow',
+    '-fsanitize=address,undefined',
+    '-fstack-protector',
+    '-fno-omit-frame-pointer',
+    '-g',
+    '-DLOCAL',
+}
 
-    vim.cmd('50vsplit output.txt')
+local types = { 'usaco', 'cf' }
+
+vim.api.nvim_create_user_command('CP', function(opts)
+    local type_ = opts.args
+    if not vim.tbl_contains(types, type_) then
+        vim.notify_once(
+            ('Must specify competition of type: []'):format(
+                table.concat(types, ', ')
+            ),
+            vim.log.levels.ERROR
+        )
+    end
+
+    vim.diagnostic.enable(false)
+    vim.api.nvim_input('i' .. type_ .. '<c-s>')
+
+    local input, output = 'input.txt', 'output.txt'
+    if type_ == 'usaco' then
+        local filename = vim.fn.expand('%:t:r')
+        input, output = filename .. '.in', filename .. '.out'
+    end
+
+    vim.cmd('50vsplit ' .. output)
     clearcol()
-    vim.cmd.split('input.txt')
+    vim.cmd.split(input)
     clearcol()
     vim.cmd.wincmd('h')
     vim.cmd('vertical resize +8')
@@ -19,8 +52,12 @@ vim.api.nvim_create_user_command('CP', function()
         buffer = vim.api.nvim_get_current_buf(),
         callback = function()
             vim.fn.jobstart(
-                ('g++ %s 2>output.txt && test -f a.out && cat input.txt | ./a.out >output.txt && rm a.out'):format(
-                    vim.fn.expand('%')
+                ('g++ %s %s 2>%s && test -f a.out && cat %s | ./a.out >%s && rm a.out'):format(
+                    table.concat(compile_flags, ' '),
+                    vim.fn.expand('%'),
+                    output,
+                    input,
+                    output
                 ),
                 {
                     on_exit = function(_, exit_code, _)
@@ -34,4 +71,4 @@ vim.api.nvim_create_user_command('CP', function()
             )
         end,
     })
-end, {})
+end, { nargs = 1 })
--- nvim/lua/snippets/cpp.lua
+++ nvim/lua/snippets/cpp.lua
@@ -1,23 +1,59 @@
-return {
-    s('in', fmt('#include {}', { i(1) })),
-    s('main', fmt('#include <iostream>\n\nint main() {{\n\t{}\n}}', { i(1) })),
-    s('pr', fmt('std::cout << {}', { i(1) })),
-    s('s', fmt('std::{}', { i(1) })),
-    s(
-        'cp',
-        fmt(
-            [[#include <bits/stdc++.h>
+local template = vim.split(
+    [[#include <bits/stdc++.h>
 using namespace std;
 
 #define all(x) (x).begin(), (x).end()
 #define sz(x) static_cast<int>((x).size())
 #define FOR(i, x) for (size_t i = 0; i < (x).size(); ++i)
 
-#ifndef ONLINE_JUDGE
+#ifndef LOCAL
 #define dbg(x) cerr << #x << " = " << (x) << '\n'
 #else
 #define dbg(x)
-#endif
+#endif]],
+    '\n'
+)
+
+return {
+    s('in', fmt('#include {}', { i(1) })),
+    s('main', fmt('#include <iostream>\n\nint main() {{\n\t{}\n}}', { i(1) })),
+    s('pr', fmt('std::cout << {}', { i(1) })),
+    s('s', fmt('std::{}', { i(1) })),
+    s(
+        'usaco',
+        fmt(
+            [[{}
+
+#define PROBLEM_NAME "{}"
+
+void solve() {{
+    {}
+}}
+
+int main() {{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  freopen(PROBLEM_NAME ".in", "r", stdin);
+  freopen(PROBLEM_NAME ".out", "w", stdout);
+
+  solve();
+
+  return 0;
+}}]],
+            {
+                t(template),
+                f(function()
+                    return vim.fn.expand('%:t:r')
+                end),
+                i(1),
+            }
+        )
+    ),
+    s(
+        'cf',
+        fmt(
+            [[{}
 
 void solve() {{
   {}
@@ -35,7 +71,7 @@ int main() {{
 
   return 0;
 }}]],
-            { i(1) }
+            { t(template), i(1) }
         )
     ),
 }
