f21302baff510d7cf36c0353031b7407e5a20e3a
Barrett Ruth <br.barrettruth@gmail.com>
2025-01-18 15:20:36 -0500

    feat(nvim): upgrade cp setup

--- nvim/lua/cp.lua
+++ nvim/lua/cp.lua
@@ -39,7 +39,8 @@ vim.api.nvim_create_user_command('CP', function(opts)
     -- Update compile flags & format config
     -- Do this first so LSP reads & configures
     vim.fn.jobstart(
-        ('test -f compile_flags.txt || echo "%s" >compile_flags.txt; test -f .clang-format || echo "%s" >.clang-format'):format(
+        ('test -f compile_flags.txt || echo "%s" >compile_flags.txt; test -f .clang-format || echo "%s" >.clang-format')
+        :format(
             compile_flags,
             clang_format
         ),
@@ -52,16 +53,16 @@ vim.api.nvim_create_user_command('CP', function(opts)
 
     -- vim.diagnostic.enable(false)
 
-    -- Populate coding buffer if empty
+    -- Populate coding buffer
     if vim.api.nvim_buf_get_lines(0, 0, -1, true)[1] == '' then
         vim.api.nvim_input('i' .. type_ .. '<c-s>')
     end
 
     -- Configure window
     local filename = vim.fn.expand('%')
-    local base_filename = vim.fn.expand('%:p:r')
+    local base_filepath = vim.fn.expand('%:p:r')
     local exename = vim.trim(vim.fn.system('mktemp'))
-    local input, output = base_filename .. '.in', base_filename .. '.out'
+    local input, output = base_filepath .. '.in', base_filepath .. '.out'
 
     vim.cmd('50vsplit ' .. output)
     vim.cmd.w()
@@ -72,82 +73,98 @@ vim.api.nvim_create_user_command('CP', function(opts)
     vim.cmd.wincmd('h')
     vim.cmd('vertical resize +8')
 
-    local output_buf = find_buf(output)
+    local input_buf, output_buf = find_buf(input), find_buf(output)
 
-    vim.api.nvim_create_autocmd('BufWritePost', {
-        buffer = vim.api.nvim_get_current_buf(),
-        callback = function()
-            vim.lsp.buf.format({ async = true })
+    -- Configure keymaps
+    local function move_problem(delta)
+        local base_filename = vim.fn.fnamemodify(base_filepath, ':t')
+        local delta_filename = (
+            string.char(base_filename:byte() + delta) .. '.cc'
+        )
+        vim.cmd.wall()
+        vim.cmd.bwipeout(input_buf)
+        vim.cmd.bwipeout(output_buf)
+        vim.cmd.e(delta_filename)
+        vim.cmd.CP(type_)
+    end
 
-            -- Compile
-            vim.fn.jobstart(
-                ('test -f %s && echo >%s; g++ @compile_flags.txt %s -o %s 2>%s'):format(
-                    output,
-                    output,
-                    filename,
-                    exename,
-                    output,
-                    output
-                ),
-                {
-                    on_exit = function()
-                        -- Send stderr to output on failure
-                        -- Configure timer
-                        local job_stopped = false
-                        local timer = vim.loop.new_timer()
+    vim.keymap.set('n', ']]', function()
+        move_problem(1)
+    end, { buf = opts.buf })
+    vim.keymap.set('n', '[[', function()
+        move_problem(-1)
+    end, { buf = opts.buf })
 
-                        -- Run the program on success
-                        vim.schedule(function()
-                            local job_id = vim.fn.jobstart(
-                                ('%s < %s >>%s 2>&1 && rm %s'):format(
-                                    exename,
-                                    input,
-                                    output,
-                                    exename
-                                ),
-                                {
-                                    on_exit = function()
-                                        if not job_stopped then
-                                            timer:stop()
-                                            timer:close()
-                                            vim.api.nvim_buf_call(
-                                                output_buf,
-                                                function()
-                                                    vim.cmd('e!')
-                                                end
-                                            )
-                                        end
-                                    end,
-                                }
-                            )
+    vim.keymap.set('n', '<leader>w', function()
+        vim.cmd.wall()
+        vim.lsp.buf.format({ async = true })
 
-                            -- Enforce timeout (2s)
-                            timer:start(2000, 0, function()
-                                vim.schedule(function()
-                                    local line_count =
-                                        vim.api.nvim_buf_line_count(output_buf)
-                                    if
-                                        vim.fn.jobwait({ job_id }, 0)[1] == -1
-                                    then
-                                        job_stopped = true
-                                        vim.fn.jobstop(job_id)
-                                        vim.api.nvim_buf_set_lines(
+        -- Compile
+        vim.fn.jobstart(
+            ('test -f %s && echo >%s; g++ @compile_flags.txt %s -o %s 2>%s'):format(
+                output,
+                output,
+                filename,
+                exename,
+                output,
+                output
+            ),
+            {
+                on_exit = function()
+                    -- Send stderr to output on failure
+                    -- Configure timer
+                    local job_stopped = false
+                    local timer = vim.loop.new_timer()
+
+                    -- Run the program on success
+                    vim.schedule(function()
+                        local job_id = vim.fn.jobstart(
+                            ('%s < %s >>%s 2>&1 && rm %s'):format(
+                                exename,
+                                input,
+                                output,
+                                exename
+                            ),
+                            {
+                                on_exit = function()
+                                    if not job_stopped then
+                                        timer:stop()
+                                        timer:close()
+                                        vim.api.nvim_buf_call(
                                             output_buf,
-                                            line_count,
-                                            line_count,
-                                            true,
-                                            { 'TIMEOUT' }
+                                            function()
+                                                vim.cmd('e!')
+                                            end
                                         )
                                     end
+                                end,
+                            }
+                        )
 
-                                    timer:stop()
-                                    timer:close()
-                                end)
+                        -- Enforce timeout (2s)
+                        timer:start(2000, 0, function()
+                            vim.schedule(function()
+                                local line_count =
+                                    vim.api.nvim_buf_line_count(output_buf)
+                                if vim.fn.jobwait({ job_id }, 0)[1] == -1 then
+                                    job_stopped = true
+                                    vim.fn.jobstop(job_id)
+                                    vim.api.nvim_buf_set_lines(
+                                        output_buf,
+                                        line_count,
+                                        line_count,
+                                        true,
+                                        { 'TIMEOUT' }
+                                    )
+                                end
+
+                                timer:stop()
+                                timer:close()
                             end)
                         end)
-                    end,
-                }
-            )
-        end,
-    })
+                    end)
+                end,
+            }
+        )
+    end, { buf = opts.buf })
 end, { nargs = 1 })
