59d1e05691c862cb8cfb9e84b2ff47e16ff111ce
Barrett Ruth <br.barrettruth@gmail.com>
2025-11-13 15:32:22 -0500

    clenaup

--- nvim/lua/plugins/nvim.lua
+++ nvim/lua/plugins/nvim.lua
@@ -514,4 +514,29 @@ return {
             vim.cmd.colorscheme(theme)
         end,
     },
+    {
+        'krady21/compiler-explorer.nvim',
+        keys = {
+            {
+                '<leader>C',
+                function()
+                    local cmd = { 'CECompileLive', 'compiler=g143' }
+                    if vim.fn.filereadable('compile_flags.txt') == 1 then
+                        for line in io.lines('compile_flags.txt') do
+                            if line ~= '' then
+                                table.insert(cmd, 'flags=' .. line)
+                            end
+                        end
+                    end
+                    vim.cmd(table.concat(cmd, ''))
+                end,
+            },
+        },
+        opts = {
+            line_match = {
+                highlight = true,
+                jump = true,
+            },
+        },
+    },
 }
